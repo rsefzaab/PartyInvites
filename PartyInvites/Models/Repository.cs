d606e3f6de3a805871e7aa71ef3019e5d19fa36a
reza <rsefzaab@yahoo.com>
2018-06-02 22:22:22 +0430

    simple project with asp.net core 2

--- PartyInvites/Models/Repository.cs
+++ PartyInvites/Models/Repository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using System.Collections.Generic;
 namespace PartyInvites.Models
 {
-    public class Repository
+    public static class Repository
     {
-        public Repository()
+        private static List<GuestResponse> responses = new List<GuestResponse>();
+
+        public static IEnumerable<GuestResponse> Responses => responses;
+
+        public static void AddResponse(GuestResponse response)
         {
+            responses.Add(response);
         }
     }
 }
